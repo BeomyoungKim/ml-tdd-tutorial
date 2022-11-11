766e7ac76367dc6cbe55ef5e23ea6aac355062e3
beomyoung <dglidgli97@gmail.com>
2022-11-11 13:52:21 +0900

    set test tensorboard log dir

--- tests/models/test_network.py
+++ tests/models/test_network.py
@@ -13,7 +13,12 @@ def test_network():
     input = torch.rand((10, 1, 28, 28))
     assert net(input).size() == torch.Size([10, 10])
 
+    for name, param_tensor in net.state_dict().items():
+        # OrderedDict 
+        print(name, "\t", param_tensor.size())
+
     for name, param in net.named_parameters():
+        # Iterator
         print(f'{name} : {param.size()}')
 
     print(f'network total params : {sum(p.numel() for p in net.parameters() if p.requires_grad)}')
@@ -21,6 +26,6 @@ def test_network():
 
     summary(net, (1, 28, 28), batch_size=1)
 
-    writer = SummaryWriter()
+    writer = SummaryWriter('runs/tests')
     writer.add_graph(net, input)
     writer.close()
